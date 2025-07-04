d277535942c6129ff17c496883913dc373f3ae29
GuillaumePons63 <guillaume@web-63.fr>
2025-07-04 07:52:21 +0200

    ajout index et config page

---
+++ residig/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home">
+        <h1>Bienvenue sur Residig</h1>
+        <p>Ceci est la page d'accueil.</p>
+    </div>
+</template>
+
+<script setup>
+// Pas de logique pour cette page simple
+</script>
+
+<style scoped>
+.home {
+    max-width: 600px;
+    margin: 40px auto;
+    text-align: center;
+}
+</style>
