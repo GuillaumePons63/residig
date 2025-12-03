494ea36f07130a168eb5321db33464df557273bd
Guillaume Pons <contact@guillaumepons.fr>
2025-12-03 18:33:04 +0100

    Add email configuration to nuxt.config.ts

--- residig/nuxt.config.ts
+++ residig/nuxt.config.ts
@@ -11,6 +11,7 @@ export default defineNuxtConfig({
     ['nuxt-mail', 
       {
          message: {
+           from: process.env.MAIL_TO,
         to: process.env.MAIL_TO,
       },
       smtp: {
